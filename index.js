14075023b78346f2c320230942ad6c1a801deb41
DavidWolff218 <dcwolff218@gmail.com>
2023-03-14 12:23:40 -0500

    tested while loop

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+
+function writeCards(array, event){
+  let newArray = []
+  let i = 0
+  while (i < array.length) {
+  	newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+	i++
+  }
+  return newArray
+}
